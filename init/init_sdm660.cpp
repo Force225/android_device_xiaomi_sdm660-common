25795dab4a0451bc8b4e9bdcf72ec5f8aefc9519
Volodymyr Zhdanov <wight554@gmail.com>
2020-09-15 00:54:16 +0300

    sdm660-common: init: Replace property_set() with android::base::SetProperty()
    * follow up https://android.googlesource.com/platform/system/core/+/c88d8f93cfaa8cbf368a0fa94e8bc84bc55c3ece

--- init/init_sdm660.cpp
+++ init/init_sdm660.cpp
@@ -41,12 +41,11 @@
 #include <sys/_system_properties.h>
 
 #include "vendor_init.h"
-#include "property_service.h"
 
 using android::base::GetProperty;
-using android::init::property_set;
 using android::base::ReadFileToString;
 using android::base::Trim;
+using android::base::SetProperty;
 
 char const *heapstartsize;
 char const *heapgrowthlimit;
@@ -113,13 +112,13 @@ void vendor_load_persist_properties()
         property_override ("persist.vendor.audio.calfile6","/vendor/etc/acdbdata/QRD/sdm660-snd-card-skush/QRD_SKUSH_Speaker_cal.acdb");
         property_override ("persist.vendor.audio.calfile7","/vendor/etc/acdbdata/QRD/sdm660-snd-card-skush/QRD_SKUSH_workspaceFile.qwsp");
         property_override ("persist.vendor.audio.calfile8","/vendor/etc/acdbdata/adsp_avs_config.acdb");
-        
+
     }
   }
 }
 void vendor_load_properties()
 {
-   std::string product = GetProperty("ro.product.vendor.device", "");	
+   std::string product = GetProperty("ro.product.vendor.device", "");
    if (product.find("whyred") != std::string::npos)
    {
   	std::string region = GetProperty("ro.boot.hwc", "");
@@ -137,4 +136,11 @@ void vendor_load_properties()
         property_override_dual("ro.product.vendor.model", "persist.vendor.camera.exif.model", "Redmi Note 5 Pro");
 	}
   }
+
+  SetProperty("dalvik.vm.heapstartsize", heapstartsize);
+  SetProperty("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
+  SetProperty("dalvik.vm.heapsize", heapsize);
+  SetProperty("dalvik.vm.heaptargetutilization", heaptargetutilization);
+  SetProperty("dalvik.vm.heapminfree", heapminfree);
+  SetProperty("dalvik.vm.heapmaxfree", heapmaxfree);
 }
