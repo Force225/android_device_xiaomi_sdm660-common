39f6a7437fbd64ef780afc33d441b309d7ae6cb0
Demon000 <demonsingur@gmail.com>
2020-09-16 23:54:41 +0530

    sdm660-common: Add shim for libdpmframework
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -559,6 +559,10 @@ PRODUCT_HOST_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(COMMON_PATH)
 
+# Shims
+PRODUCT_PACKAGES += \
+    libdpmframework_shim
+
 # Tetheroffload
 PRODUCT_PACKAGES += \
     ipacm \
